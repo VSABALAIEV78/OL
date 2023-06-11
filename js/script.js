56f2512d5ce1518337ff6cf1f44989d106de2589
Vadik <sabalayev1@gmail.com>
2023-06-11 20:31:51 +0300

    Added services and active class

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    //change active class for .catalog_item
+    $('div.service_wrapper').on('click', 'div:not(div.catalog_item_active, div.item__title)', function () {
+        $(this)
+            .addClass('catalog_item_active').siblings().removeClass('catalog_item_active')
+            .closest('div.container').find('div.service_list').removeClass('service_list_active').eq($(this).index()).addClass('service_list_active');
+    });
+
+
+});
